b197ca612e210220eead2523d7f33ee20a2af816
liuxiao <liuxiao1998@sjtu.edu.cn>
2017-07-24 20:03:41 +0800

    11

--- bqg.py
+++ bqg.py
@@ -12,16 +12,18 @@ title=titlelist[0].decode('gb2312','ignore').encode('utf8','ignore')
 print len(namelist),len(linklist)
 k=0
 os.system('mkdir ./%s'%title)
-with open('./%s/%s.txt'%(title,title),'w') as fp:
-    fp.write('%s\n'%title)
-    for name in namelist:
-        fp.write('%s\n'%namelist[k].decode('gb2312','ignore').encode('utf8','ignore'))
-        url='http://www.biquzi.com%s'%linklist[k].decode('gb2312','ignore').encode('utf8','ignore')
-        page=data.GetPage(url,None)
-        conlist=data.FindPat(content_pat,page)
-        for lines in conlist:
-            fp.write('    %s\n'%lines.decode('gb2312','ignore').encode('utf8','ignore'))
-        k+=1
-        fp.write('\n\n\n\n')
-        print '第%d章结束'%(k)
-        time.sleep(1)
+try:
+    with open('./%s/%s.txt'%(title,title),'w') as fp:
+        fp.write('%s\n'%title)
+        for name in namelist:
+            fp.write('%s\n'%namelist[k].decode('gb2312','ignore').encode('utf8','ignore'))
+            url='http://www.biquzi.com%s'%linklist[k].decode('gb2312','ignore').encode('utf8','ignore')
+            page=data.GetPage(url,None)
+            conlist=data.FindPat(content_pat,page)
+            for lines in conlist:
+                fp.write('    %s\n'%lines.decode('gb2312','ignore').encode('utf8','ignore'))
+            k+=1
+            print '第%d章结束'%(k)
+            fp.write('\n\n\n\n')
+except Exception as e:
+    print e
